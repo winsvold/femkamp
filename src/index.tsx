84db7cd8f0d3309876c6a7744d25b95ec9220b8e
Daniel Winsvold <danielwinsvold@gmail.com>
2020-03-29 21:44:46 +0200

    legger til fast-refresh

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import './index.css';
 import './reset.css';
 import App, { basePath } from './App/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { Runder } from './AppContext';
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <Route path={`${basePath}:runde`} component={App} />
+            <Route path={''} render={() => <Redirect to={`${basePath}${Runder[Runder.Setup]}`} />} />
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
