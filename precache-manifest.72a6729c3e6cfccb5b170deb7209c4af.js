f63993e88fa60d9ef4bd54d893a3bdff2536d913
Daniel Winsvold <danielwinsvold@gmail.com>
2020-03-29 23:42:34 +0200

    Updates

---
+++ precache-manifest.72a6729c3e6cfccb5b170deb7209c4af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625cd724cd5395f4f675add361386b22",
+    "url": "/femkamp/index.html"
+  },
+  {
+    "revision": "8c18c6024dd25b63419b",
+    "url": "/femkamp/static/css/main.200499f2.chunk.css"
+  },
+  {
+    "revision": "c6793a2ee53cb5d3f224",
+    "url": "/femkamp/static/js/2.eb5904c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/femkamp/static/js/2.eb5904c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c18c6024dd25b63419b",
+    "url": "/femkamp/static/js/main.4ed45518.chunk.js"
+  },
+  {
+    "revision": "e3a7696ac1ef41fc7ef9",
+    "url": "/femkamp/static/js/runtime-main.0cf8e4ee.js"
+  }
+]);
