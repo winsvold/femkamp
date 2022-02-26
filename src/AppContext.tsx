5adbfa0d98cecc3f19face21643d7a8567a53b5b
Daniel Winsvold <danielwinsvold@gmail.com>
2022-02-26 10:31:35 +0100

    bumper masse pakker for å gjøre snyk glad

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,121 +1,118 @@
-import React, { createContext, ReactNode, useState } from 'react';
-import { injectTotalScore } from './injectTotalScore';
-import { lagreSpill } from './App/manageHistorikk';
+import React, { createContext, ReactNode, useState } from "react";
+import { injectTotalScore } from "./injectTotalScore";
+import { lagreSpill } from "./App/manageHistorikk";
 
 export interface Score {
+  pass: number;
+  kløver: number;
+  kabal: {
     pass: number;
-    kløver: number;
-    kabal: {
-        pass: number;
-        rest: number;
-    };
-    dame: number;
-    grang: number;
-    total: number;
+    rest: number;
+  };
+  dame: number;
+  grang: number;
+  total: number;
 }
 
 export interface Spiller {
-    navn: string;
-    score: Score;
+  navn: string;
+  score: Score;
 }
 
 export enum Runder {
-    Historikk,
-    NyttSpill,
-    Pass,
-    Kløver,
-    Kabal,
-    Dame,
-    Grang,
-    Resultat,
+  Historikk,
+  NyttSpill,
+  Pass,
+  Kløver,
+  Kabal,
+  Dame,
+  Grang,
+  Resultat,
 }
 
 interface GameContextI {
-    runde: Runder;
-    spillere: Spiller[];
-    addSpiller: (navn: string) => void;
-    updateScore: (spiller: Spiller, score: Partial<Score>) => void;
-    setRunde: (runde: Runder) => void;
-    removeSpiller: (spiller: Spiller) => void;
-    lagreOgStartPåNytt: () => void;
+  runde: Runder;
+  spillere: Spiller[];
+  addSpiller: (navn: string) => void;
+  updateScore: (spiller: Spiller, score: Partial<Score>) => void;
+  setRunde: (runde: Runder) => void;
+  removeSpiller: (spiller: Spiller) => void;
+  lagreOgStartPåNytt: () => void;
 }
 
 const defaultScore: Score = {
+  pass: 0,
+  kløver: 0,
+  kabal: {
     pass: 0,
-    kløver: 0,
-    kabal: {
-        pass: 0,
-        rest: 0,
-    },
-    dame: 0,
-    grang: 0,
-    total: 0,
+    rest: 0,
+  },
+  dame: 0,
+  grang: 0,
+  total: 0,
 };
 
 const localStorage = window.localStorage;
 
-const localstorageSpillere = localStorage.getItem('spillere');
+const localstorageSpillere = localStorage.getItem("spillere");
 
 const initialState: GameContextI = {
-    spillere: localstorageSpillere ? JSON.parse(localstorageSpillere) : [],
-    runde: Runder.NyttSpill,
-    addSpiller: () => null,
-    updateScore: () => null,
-    setRunde: () => null,
-    removeSpiller: () => null,
-    lagreOgStartPåNytt: () => null,
+  spillere: localstorageSpillere ? JSON.parse(localstorageSpillere) : [],
+  runde: Runder.NyttSpill,
+  addSpiller: () => null,
+  updateScore: () => null,
+  setRunde: () => null,
+  removeSpiller: () => null,
+  lagreOgStartPåNytt: () => null,
 };
 
 export const GameContext = createContext<GameContextI>(initialState);
 
 export function GameContextProvider(props: { children: ReactNode }) {
-    const [runde, setRunde] = useState<Runder>(initialState.runde);
-    const [spillere, setSpillere] = useState<Spiller[]>(initialState.spillere);
+  const [runde, setRunde] = useState<Runder>(initialState.runde);
+  const [spillere, setSpillere] = useState<Spiller[]>(initialState.spillere);
 
-    const addSpiller = (navn: string) => {
-        setSpillere((prevState) => [
-            ...prevState.filter((it) => it.navn !== navn),
-            { navn: navn, score: defaultScore },
-        ]);
+  const addSpiller = (navn: string) => {
+    setSpillere((prevState) => [...prevState.filter((it) => it.navn !== navn), { navn: navn, score: defaultScore }]);
+  };
+
+  const updateScore = (spiller: Spiller, score: Partial<Score>) => {
+    const updatedSpiller = {
+      ...spiller,
+      score: {
+        ...spiller.score,
+        ...score,
+      },
     };
+    setSpillere((prevState) => [...prevState.filter((it) => it.navn !== spiller.navn), updatedSpiller]);
+  };
 
-    const updateScore = (spiller: Spiller, score: Partial<Score>) => {
-        const updatedSpiller = {
-            ...spiller,
-            score: {
-                ...spiller.score,
-                ...score,
-            },
-        };
-        setSpillere((prevState) => [...prevState.filter((it) => it.navn !== spiller.navn), updatedSpiller]);
-    };
+  const removeSpiller = (spiller: Spiller) => {
+    setSpillere((prevState) => [...prevState.filter((it) => it.navn !== spiller.navn)]);
+  };
 
-    const removeSpiller = (spiller: Spiller) => {
-        setSpillere((prevState) => [...prevState.filter((it) => it.navn !== spiller.navn)]);
-    };
+  const spillereMedTotalScore = injectTotalScore(spillere);
 
-    const spillereMedTotalScore = injectTotalScore(spillere);
+  localStorage.setItem("spillere", JSON.stringify(spillereMedTotalScore));
+  const lagreOgStartPåNytt = () => {
+    lagreSpill(spillereMedTotalScore);
+    setSpillere([]);
+    setRunde(Runder.NyttSpill);
+  };
 
-    localStorage.setItem('spillere', JSON.stringify(spillereMedTotalScore));
-    const lagreOgStartPåNytt = () => {
-        lagreSpill(spillereMedTotalScore);
-        setSpillere([]);
-        setRunde(Runder.NyttSpill);
-    };
-
-    return (
-        <GameContext.Provider
-            value={{
-                setRunde,
-                spillere: spillereMedTotalScore,
-                runde,
-                lagreOgStartPåNytt,
-                addSpiller,
-                updateScore,
-                removeSpiller,
-            }}
-        >
-            {props.children}
-        </GameContext.Provider>
-    );
+  return (
+    <GameContext.Provider
+      value={{
+        setRunde,
+        spillere: spillereMedTotalScore.sort((a, b) => (a.navn === b.navn ? 0 : a.navn < b.navn ? -1 : 1)),
+        runde,
+        lagreOgStartPåNytt,
+        addSpiller,
+        updateScore,
+        removeSpiller,
+      }}
+    >
+      {props.children}
+    </GameContext.Provider>
+  );
 }
